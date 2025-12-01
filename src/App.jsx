a3e18f5d4de42de3cc299450b7aa47f294e0e73b
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-12-01 15:55:27 +0100

    feat: creata struttura base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import articoli from './assets/articoli'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newArticolo, setNewArticolo] = useState(null)
 
   return (
     <>
+      <header>
+        <nav className="navbar bg-body-tertiary">
+          <h1>
+            ARTICOLI
+          </h1>
+        </nav>
+      </header>
 
+      <main>
+        {articoli.map((articolo) => {
+          return (
+            <div className="alert alert-light">
+              {articolo}
+            </div>
+          )
+        })}
+
+
+        <label for="inputArticolo" className="form-label">Inserisci un nuovo articolo</label>
+        <input type="text" id="inputArticolo" className="form-control"></input>
+      </main>
     </>
   )
 }
---
+++ src/assets/articoli.js
@@ -0,0 +1,3 @@
+const articoli = ["ARTICOLO 1", "ARTICOLO2", "ARTICOLO 3", "ARTICOLO 4"]
+
+export default articoli
